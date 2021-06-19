1a4bc967826d84c20e8dcdbe0c918953a81dcfa4
Sahil <56653026+sahilg50@users.noreply.github.com>
2021-06-20 02:00:56 +0530

    day 3

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -4,11 +4,15 @@ def sort(array):
 
     for i in range(len(array)-1):
 
+        # Find the minimum element in remaining
+        # unsorted array
         min_idx = i
         for j in range(i+1, len(array)):
             if array[min_idx] > array[j]:
                 min_idx = j
 
+        # Swap the found minimum element with
+        # the first element
         array[min_idx], array[i] = array[i], array[min_idx]
 
     print(array)
