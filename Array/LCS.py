4bb001914141ebe17abe5e47270b7acbb428a284
Sahil <56653026+sahilg50@users.noreply.github.com>
2021-06-27 17:50:58 +0530

    Added Memozation to recursion

--- Array/LCS.py
+++ Array/LCS.py
@@ -6,12 +6,16 @@ LCS for input Sequences “ABCDGH” and “AEDFHR” is “ADH” of length 3.
 LCS for input Sequences “AGGTAB” and “GXTXAYB” is “GTAB” of length 4.
 """
 
+# To improve the results of recursion we can use the process of memoization
+
 
 class Strings:
 
-    def __init__(self) -> None:
+    def __init__(self):
         self.str1 = input("Enter the first string: ")
         self.str2 = input("Enter the secong string: ")
+        self.memoization_array = [
+            [-1 for i in range(100)] for j in range(100)]
 
     def LCS(self, i=0, j=0):
 
@@ -19,15 +23,22 @@ class Strings:
         if(i >= len(self.str1) or j >= len(self.str2)):
             return 0
 
+        # If the same state has already been computed
+        if(self.memoization_array[i][j] != -1):
+            return self.memoization_array[i][j]
+
+        # if equal, then we store the value of the function call
         elif(self.str1[i] == self.str2[j]):
-            return 1 + self.LCS(i+1, j+1)
+            self.memoization_array[i][j] = 1 + self.LCS(i+1, j+1)
+            return self.memoization_array[i][j]
 
         else:
-            return max(self.LCS(i+1, j), self.LCS(i, j+1))
+            self.memoization_array[i][j] = max(
+                self.LCS(i+1, j), self.LCS(i, j+1))
+            return self.memoization_array[i][j]
 
 
 def main():
-
     strings = Strings()
     print(strings.LCS())
 
